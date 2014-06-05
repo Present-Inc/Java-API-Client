2c035f9fe4260899255f0ae57090eb9123a9f709
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-05 12:47:17 -0400

    Initial commit

---
+++ src/tv/present/enumerations/SubjectiveMetaDirection.java
@@ -0,0 +1,11 @@
+package tv.present.enumerations;
+
+/**
+ * Present Subjective Meta Direction Types
+ * June 05, 2014
+ * @author Kyle Weisel (kyle@present.tv)
+ */
+public enum SubjectiveMetaDirection {
+    Backward,
+    Forward
+}
