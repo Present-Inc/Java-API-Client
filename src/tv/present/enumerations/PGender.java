25fa63641ed1e26908677fd50960ff50590aefce
Kyle Weisel <kyle.weisel@gmail.com>
2014-06-06 19:38:28 -0400

    Initial commit

---
+++ src/tv/present/enumerations/PGender.java
@@ -0,0 +1,12 @@
+package tv.present.enumerations;
+
+/**
+ * Present Gender Types
+ * June 05, 2014
+ * @author Kyle Weisel (kyle@present.tv)
+ */
+public enum PGender {
+    Female,
+    Male,
+    Unspecified
+}
